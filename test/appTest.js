9941c030fc336de9d03ca7a082fae267df9be38f
Felipe França <felipejonataoliveira@gmail.com>
2017-12-26 19:00:30 -0200

    Refactoring - separating the tests by group of tests

--- test/appTest.js
+++ test/appTest.js
@@ -1,26 +1,29 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
+const app = require('../app');
+
+
+//Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5,5);
 
 describe('App', function(){
-	it('sayHello should return hello', function(){
-        let result = sayHello();
-		assert.equal(result, 'hello');
+
+	describe('sayHello()', function(){
+		it('sayHello should return hello', function(){
+			assert.equal(sayHelloResult, 'hello');
+		});
+		it('sayHello should return type string', function(){
+	    assert.typeOf(sayHelloResult, 'string');
+		});
 	});
 
-	it('sayHello should return type string', function(){
-        let result = sayHello();
-        assert.typeOf(result, 'string');
-	});
+	describe('addNumbers()', function(){
+		it('addNumbers shold be above 5', function(){
+			assert.isAbove(addNumbersResult, 9);
+		});
 
-	it('addNumbers shold be above 5', function(){
-        let result = addNumbers(5,5);
-        assert.isAbove(result, 9);
+		it('addNumber should return type number', function(){
+			assert.typeOf(addNumbersResult, 'number');
+		});
 	});
-
-	it('addNumber should return type number', function(){
-        let result = addNumbers(5,5);
-        assert.typeOf(result, 'number');
-    });
 });
-
